3d876b310735d8a35572644b9c5d6a1f800b017f
itzezc <chirub@foretag.co>
2021-01-20 17:56:23 +0000

    [ORM] Persistence save and delete methods

--- source/Leon/ORM/Persistence.cr
+++ source/Leon/ORM/Persistence.cr
@@ -1,27 +1,55 @@
+require "json"
+
 module Leon
 	module ORM
 		module Persistence
 			macro __process_persistence
 
 				def save
-					# begin
-					# 	if _key
-					# 		@updated_at = Time.now
-					# 		# Update
-					# 	else
-					# 		@created_at = Time.now
-					# 		@updated_at = Time.now
-					# 	end
+					begin
+						if _key
+							# @updated_at = Time.now
+							# Update
 
-					# 	return true
-					# rescue ex
+							unless self.updated_at.nil?
+								self.updated_at = Time.utc
+							end
 
-					# 	return false
-					# end
-					puts "save"
+							puts "Key exists"
+						else
+							unless @@database.nil?
+								attributes = self.attributes
+								attributes.delete(:_key)
+
+								obj = self.collection.document.create(
+									Hash(String, String | Array(String) | Int32 | Nil).from_json(attributes.to_json)
+								)
+
+								self._key = obj["_key"].to_s
+
+								return self.attributes
+							end
+						end
+
+						return false
+					rescue exception
+						puts exception
+						return false
+					end
 				end
 
 				def delete
+					begin
+						unless @@database.nil?
+							# TODO: Check for soft deletes
+							obj = self.collection.document.delete(self._key.as(String))
+							return true
+						end
+						return false
+					rescue exception
+						puts exception
+						return false
+					end
 				end
 
 				def force_delete
