833fef6203382641e530509453844f1c76cd6069
itzezc <chirub@foretag.co>
2021-01-20 00:30:41 +0000

    Leon yield configuration

--- source/leon.cr
+++ source/leon.cr
@@ -2,5 +2,15 @@ require "./Leon/*"
 require "./Leon/ORM/*"
 
 module Leon
-  VERSION = "0.1.2"
+	VERSION = "0.1.2"
+
+  	CONNECTION = Connection.new
+
+	def self.connection
+		Connection
+	end
+
+	def self.configuration
+		yield Connection
+	end
 end
