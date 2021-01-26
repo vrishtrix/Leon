9aa0f904be237d095184cb0f8317e30d7a43b1f3
itzezc <chirub@foretag.co>
2021-01-26 01:18:52 +0000

    [ORM] Operations self .all, .find

--- source/Leon/ORM/Operations.cr
+++ source/Leon/ORM/Operations.cr
@@ -4,35 +4,51 @@ module Leon
 			macro __process_operations
 
 				def self.all
+					# TODO: Refactor and return Array(self)
+					self.collection.all_documents["result"]
 				end
 
-				def self.find(id : String)
-					begin
-						unless @@database.nil?
-							obj = collection.document.get(id)
+				def self.find(id : String) : self
+					# TODO: Refactor with guards
+					obj = collection.document.get(id)
+					instance = self.new
+					instance._key = obj["_key"].to_s
+					instance.assign_attributes_from_json(obj.to_json)
+					instance
+				end
 
-							attributes = Hash(String, String | Array(String) | Int32 | Nil | Bool | Int64 | Float32 | Float64).from_json(obj.to_json)
+				def self.where(filters : Hash(String, String | Bool | Float64 | Int32 | Int64 | Array(String) | Array(Int32))) # : self | Array(self)
 
-							return Nil if attributes.has_key?("code")
-
-							instance = self.new
-							instance.assign_attributes_from_json(obj.to_json)
-
-							return instance
-
-							return Nil
+					query = String.build do | io |
+						io << "FOR x IN #{self.get_collection}"
+						io << "\n"
+							io << "	FILTER "
+						filters.each do | key, value |
+							io << "x.#{key} == @#{key} #{key == filters.last_key? ? "" : "&& "}"
 						end
-					rescue exception
-						puts exception
-						return Nil
+						io << "\n"
+						io << "	RETURN x"
 					end
+
+					puts "Query: #{query}"
+
+					aql = self.aql
+					cursor = aql.cursor({
+						"query" => query,
+						"bindVars": {
+							email: "chirub@foretag.co"
+						},
+						"count": true
+					})
+
+					puts cursor["result"]
+
 				end
 
-				def self.where
-					puts "where"
-				end
-
-				def self.destroy
+				def self.destroy(id : String)
+					obj = self.collection.document.delete(id)
+					puts obj
+					return true
 				end
 
 			end
